78df4f6b31516cba78025ab857a4c6ac525df7f9
divyasharma <divya030401@gmail.com>
2023-09-28 17:30:53 +0530

    added style in login page

--- index.php
+++ index.php
@@ -3,13 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
-    <form method="post" action="login.php">
-        <input type="text" name="username" required placeholder="Username"><br>
-        <input type="password" name="password" required placeholder="password"><br>
-        <input type="submit" value="Login" name="login_btn">
-    </form>
+    <div class="center">
+        <h1>Login</h1>
+        <div class="form">
+            <form  method="post" action="login.php">
+            <input type="text" name="username" class="textfield" required placeholder="Username"><br>
+            <input type="password" name="password" class="textfield" required placeholder="password"><br>
+            <input type="submit" value="Login" name="login_btn" class="btn">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
